c15eedd16ca88471d12e25830cd60455c3cf82e3
Ridz Balla <ridz@payble.com.au>
2023-02-13 10:15:32 +1100

    added sst notes app code

---
+++ services/functions/get.js
@@ -0,0 +1,22 @@
+import handler from '../util/handler';
+import dynamoDb from '../util/dynamodb';
+
+// Wrong handler function name
+export const main = handler(async (event) => {
+  const params = {
+    TableName: process.env.TABLE_NAME,
+    // 'Key' defines the partition key and sort key of the item to be retrieved
+    Key: {
+      userId: event.requestContext.authorizer.iam.cognitoIdentity.identityId, // The id of the author
+      noteId: event.pathParameters.id, // The id of the note from the path
+    },
+  };
+
+  const result = await dynamoDbLib.call('get', params);
+  if (!result.Item) {
+    throw new Error('Item not found.');
+  }
+
+  // Return the retrieved item
+  return result.Item;
+});
---
+++ stacks/index.js
@@ -0,0 +1,20 @@
+import { AuthStack } from "./AuthStack";
+import { StorageStack } from "./StorageStack";
+import { ApiStack } from "./ApiStack";
+import { FrontendStack } from "./FrontendStack";
+
+export default function main(app) {
+  app.setDefaultFunctionProps({
+    runtime: "nodejs16.x",
+    srcPath: "services",
+    bundle: {
+      format: "esm",
+    },
+  });
+  app
+  
+  .stack(StorageStack)
+  .stack(ApiStack)
+  .stack(AuthStack)
+  .stack(FrontendStack);
+}
